e5e0c8eb516b9aef1b9bc05558c71f132625cda8
joonseosa <myeongjun1722@naver.com>
2021-08-09 06:00:12 +0900

    JS: navbar menu click
    navbar menu click 시 이동

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ document.addEventListener('scroll',()=>{
 
 
 });
+
+// tab선택시 스크롤링 이동 
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=> {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+
+});
